af622169920a582c05d525c84d8591cb0659ebcc
V <78769380+codegod100@users.noreply.github.com>
2023-07-27 00:31:36 +0000

    update svelte.config.js

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,4 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-cloudflare';
 // import adapter from '@sveltejs/adapter-node';
 // import adapter from '@sveltejs/adapter-static';
 
